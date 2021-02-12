846b79739bd8f0fd8cb35ef0e9e8b46c794d2cfa
Mahathir <mirmahathir1@gmail.com>
2021-02-12 17:38:30 +0600

    deploying to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,5 +4,5 @@ var mongoose=require('mongoose');
 mongoose.Promise=global.Promise;
 //mongoose.connect('mongodb://localhost:27017/Keep');
 //mongodb+srv://taaha:<password>@cluster0-0zww4.mongodb.net/test?retryWrites=true&w=majority
-mongoose.connect('mongodb+srv://taaha:taaha@cluster0-0zww4.mongodb.net/Keep?retryWrites=true&w=majority');
+mongoose.connect(process.env.MONGO_URL);
 module.exports={mongoose};
