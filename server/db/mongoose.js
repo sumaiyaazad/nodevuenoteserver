ad219ec1306258ed59f34df14fc27668342a74b6
sumaiyaazad <azadsumaiya00@gmail.com>
2021-05-28 20:46:09 +0600

    dokcer version 2 working

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,6 +5,7 @@ mongoose.Promise=global.Promise;
 let connectionString=process.env.MONGO_URL|| 'mongodb://mongo:27017/Keep';
 //mongoose.connect('mongodb://localhost:27017/Keep');
 //mongodb+srv://taaha:<password>@cluster0-0zww4.mongodb.net/test?retryWrites=true&w=majority
+console.log(connectionString);
 mongoose.connect(connectionString);
 //mongoose.connect('mongodb+srv://mt-notebook:mt-notebook@cluster0.0zww4.mongodb.net/Notebook?retryWrites=true&w=majority');
 module.exports={mongoose};
