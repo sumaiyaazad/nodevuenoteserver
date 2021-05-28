b054ca0c661bf39698ba4ce585669de8b37178da
sumaiyaazad <azadsumaiya00@gmail.com>
2021-05-28 14:17:20 +0600

    docker version 2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,8 +2,9 @@
 var mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
+let connectionString=process.env.MONGO_URL|| 'mongodb://mongo:27017/Keep';
 //mongoose.connect('mongodb://localhost:27017/Keep');
 //mongodb+srv://taaha:<password>@cluster0-0zww4.mongodb.net/test?retryWrites=true&w=majority
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(connectionString);
 //mongoose.connect('mongodb+srv://mt-notebook:mt-notebook@cluster0.0zww4.mongodb.net/Notebook?retryWrites=true&w=majority');
 module.exports={mongoose};
