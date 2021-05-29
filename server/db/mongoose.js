72344202c411a664b14711c065bc7c4ed1e27bb0
sumaiyaazad <azadsumaiya00@gmail.com>
2021-05-29 16:40:32 +0600

    docker version 1

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,7 @@ mongoose.Promise=global.Promise;
 
 //let host=location.host.split(':')[0]
 let ip = require("ip");
-let connectionString=process.env.MONGO_URL|| 'mongodb://'+ip.address()+':27017/Notebook';
+let connectionString=process.env.MONGO_URL|| 'mongodb://mongo:27017/Notebook';
 //mongoose.connect('mongodb://localhost:27017/Keep');
 //mongodb+srv://taaha:<password>@cluster0-0zww4.mongodb.net/test?retryWrites=true&w=majority
 console.log(connectionString);
